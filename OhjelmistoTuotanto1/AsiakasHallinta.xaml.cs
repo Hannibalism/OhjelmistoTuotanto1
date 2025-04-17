ad64273ebdd564ee90d037c817b95c74db7a33a9
unknown <thehannibalism@protonmail.com>
2025-04-17 20:13:07 +0300

    Asiakashallinta sivu valmis (bugit pitää vielä tarkistaa)

--- OhjelmistoTuotanto1/AsiakasHallinta.xaml.cs
+++ OhjelmistoTuotanto1/AsiakasHallinta.xaml.cs
@@ -1,14 +1,19 @@
 namespace OhjelmistoTuotanto1;
+
 using MySqlConnector;
 using System.Collections.ObjectModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Reflection.PortableExecutable;
 
 public partial class AsiakasHallinta : ContentPage
 {
     public ObservableCollection<Asiakas> AsiakasList { get; set; }
 
 
-    /// Toistaiseksi n‰ytt‰‰ asiakastiedot tietokannasta
-    ///Lis‰ys ei viel‰ implementoitu
+    //Lis‰‰ tietokantaan asiakkaan tietoja ja p‰ivitys onnistuu valitsemalla listasta asiakas.
+    //Tiedot kopioituvat kenttiin ja asiakasID kentt‰ t‰yttyy jonka j‰lkeen kyseisen asiakasID:n
+    //Tiedot p‰ivittyv‰t samasta napista. Jos uutta tietoa haluaa asiakkaan valittua lis‰t‰
+    //Pit‰‰ kent‰t tyhjent‰‰ tyhjennys napilla.
     public AsiakasHallinta()
     {
         InitializeComponent();
@@ -33,21 +38,163 @@ public partial class AsiakasHallinta : ContentPage
                     AsiakasId = reader.GetInt32("asiakas_id"),
                     Etunimi = reader.GetString("etunimi"),
                     Sukunimi = reader.GetString("sukunimi"),
+                    Postinumero = reader.GetString("postinro"),
                     Lahiosoite = reader.GetString("lahiosoite"),
                     Email = reader.GetString("email"),
                     Puhelinnro = reader.GetString("puhelinnro")
                 });
             }
         }
-    }
-}
 
-public class Asiakas
-{
-    public int AsiakasId { get; set; }
-    public string Etunimi { get; set; }
-    public string Sukunimi { get; set; }
-    public string Lahiosoite { get; set; }
-    public string Email { get; set; }
-    public string Puhelinnro { get; set; }
+    }
+    public void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.Count > 0)
+        {
+            var selectedAsiakas = e.CurrentSelection[0] as Asiakas;
+            if (selectedAsiakas != null)
+            {
+                IDEntry.Text = selectedAsiakas.AsiakasId.ToString();
+                EtunimiEntry.Text = selectedAsiakas.Etunimi;
+                SukunimiEntry.Text = selectedAsiakas.Sukunimi;
+                PostinumeroEntry.Text = selectedAsiakas.Postinumero;
+                LahiosoiteEntry.Text = selectedAsiakas.Lahiosoite;
+                emailEntry.Text = selectedAsiakas.Email;
+                PuhelinnumeroEntry.Text = selectedAsiakas.Puhelinnro;
+            }
+        }
+    }
+    private void Tyhjenna(object sender, EventArgs e) 
+    { 
+        IDEntry.Text = string.Empty;
+        EtunimiEntry.Text = string.Empty;
+        SukunimiEntry.Text = string.Empty;
+        PostinumeroEntry.Text= string.Empty;
+        LahiosoiteEntry.Text= string.Empty;
+        emailEntry.Text= string.Empty;
+        PuhelinnumeroEntry.Text = string.Empty;
+        ToimipaikkaEntry.Text= string.Empty;
+    }
+    private async void Lisaa(object sender, EventArgs e)
+    {
+        string asiakasid = IDEntry.Text;
+        string etunimi = EtunimiEntry.Text;
+        string sukunimi = SukunimiEntry.Text;
+        string postinumero = PostinumeroEntry.Text;
+        string lahiosoite = LahiosoiteEntry.Text;
+        string email = emailEntry.Text;
+        string puhelinnumero = PuhelinnumeroEntry.Text;
+        string toimipaikka = ToimipaikkaEntry.Text;
+        await InsertData(asiakasid, etunimi, sukunimi, postinumero, lahiosoite, email, puhelinnumero, toimipaikka);
+    }
+    private async Task InsertData(string asiakasid, string etunimi, string sukunimi, string postinumero, string lahiosoite, string email, string puhelinnumero, string toimipaikka)
+    {
+        if (asiakasid == null || asiakasid == string.Empty)
+        {
+            DatabaseConnection dbc = new DatabaseConnection();
+            using (var connection = dbc._getConnection())
+            {
+                connection.Open();
+                string postinroTarkistus = "SELECT COUNT(*) from vn.posti where postinro = @postinro;";
+                using (var command = new MySqlCommand(postinroTarkistus, connection))
+                {
+                    command.Parameters.AddWithValue("@postinro", postinumero);
+                    int count = Convert.ToInt32(await command.ExecuteScalarAsync());
+
+                    if (count > 0)
+                    {
+                        await DisplayAlert("Oho", "Postinumero on jo kannassa, k‰ytet‰‰n olevaa tietoa", "ok");
+                    }
+                    else if (count == 0)
+                    {
+                        string addPostinro = "INSERT INTO vn.posti (postinro, toimipaikka) VALUES (@postinro, @toimipaikka);";
+                        using (var insertCommand = new MySqlCommand(addPostinro, connection))
+                        {
+                            insertCommand.Parameters.AddWithValue("@postinro", postinumero);
+                            insertCommand.Parameters.AddWithValue("@toimipaikka", toimipaikka);
+                            await insertCommand.ExecuteNonQueryAsync();
+                        }
+                    }
+                }
+                string addAsiakas = "INSERT INTO vn.asiakas (postinro, etunimi, sukunimi, lahiosoite, email, puhelinnro) VALUES ((SELECT postinro from vn.posti where postinro = @postinumero), @etunimi, @sukunimi, @lahiosoite, @email, @puhelinnumero); SELECT LAST_INSERT_ID();";
+                using (var Command = new MySqlCommand(addAsiakas, connection))
+                {
+                    Command.Parameters.AddWithValue("@etunimi", etunimi);
+                    Command.Parameters.AddWithValue("@sukunimi", sukunimi);
+                    Command.Parameters.AddWithValue("@postinumero", postinumero);
+                    Command.Parameters.AddWithValue("@lahiosoite", lahiosoite);
+                    Command.Parameters.AddWithValue("@email", email);
+                    Command.Parameters.AddWithValue("@puhelinnumero", puhelinnumero);
+                    var newId = await Command.ExecuteScalarAsync();
+                    await DisplayAlert("Onnistui", "Listaan lis‰ys onnistui", "Ok");
+                    AsiakasList.Add(new Asiakas
+                    {
+                        AsiakasId = Convert.ToInt32(newId),
+                        Etunimi = etunimi,
+                        Sukunimi = sukunimi,
+                        Postinumero = postinumero,
+                        Lahiosoite = lahiosoite,
+                        Email = email,
+                        Puhelinnro = puhelinnumero
+                    });
+                }
+            }
+        }
+        else if (IDEntry.Text != null || IDEntry.Text != string.Empty)
+        {
+            DatabaseConnection dbc = new DatabaseConnection();
+            using (var connection = dbc._getConnection())
+            {
+                connection.Open();
+
+                string postinroTarkistus = "SELECT COUNT(*) from vn.posti where postinro = @postinro;";
+                using (var command = new MySqlCommand(postinroTarkistus, connection))
+                {
+                    command.Parameters.AddWithValue("@postinro", postinumero);
+                    int count = Convert.ToInt32(await command.ExecuteScalarAsync());
+
+                    if (count > 0)
+                    {
+                        await DisplayAlert("Oho", "Postinumero on jo kannassa, k‰ytet‰‰n olevaa tietoa", "ok");
+                    }
+                    else if (count == 0)
+                    {
+                        string addPostinro = "INSERT INTO vn.posti (postinro, toimipaikka) VALUES (@postinro, @toimipaikka);";
+                        using (var insertCommand = new MySqlCommand(addPostinro, connection))
+                        {
+                            insertCommand.Parameters.AddWithValue("@postinro", postinumero);
+                            insertCommand.Parameters.AddWithValue("@toimipaikka", toimipaikka);
+                            await insertCommand.ExecuteNonQueryAsync();
+                        }
+                    }
+                }
+
+                string PaivitaAsiakas = "UPDATE vn.asiakas SET postinro = @postinumero, etunimi = @etunimi, sukunimi = @sukunimi, lahiosoite = @lahiosoite, email = @email, puhelinnro = @puhelinnumero WHERE asiakas_id = @asiakasID;";
+                using (var Command = new MySqlCommand(PaivitaAsiakas, connection))
+                {
+                    Command.Parameters.AddWithValue("@asiakasID", int.Parse(IDEntry.Text));
+                    Command.Parameters.AddWithValue("@etunimi", etunimi);
+                    Command.Parameters.AddWithValue("@sukunimi", sukunimi);
+                    Command.Parameters.AddWithValue("@postinumero", postinumero);
+                    Command.Parameters.AddWithValue("@lahiosoite", lahiosoite);
+                    Command.Parameters.AddWithValue("@email", email);
+                    Command.Parameters.AddWithValue("@puhelinnumero", puhelinnumero);
+                    Command.ExecuteScalar();
+                }
+            }
+        }
+    }
+
+    public class Asiakas
+    {
+        public int AsiakasId { get; set; }
+        public string Etunimi { get; set; }
+        public string Sukunimi { get; set; }
+        public string Postinumero {  get; set; }
+        public string Lahiosoite { get; set; }
+        public string Email { get; set; }
+        public string Puhelinnro { get; set; }
+
+
+    }
 }
