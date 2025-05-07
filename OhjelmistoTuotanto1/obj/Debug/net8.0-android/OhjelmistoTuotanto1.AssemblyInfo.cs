081eb1b5947ccdc71482b937aa72a11aff4b447e
Mikko Tuovinen <s2419906@edu.savonia.fi>
2025-05-07 18:17:46 +0300

    Pyryn Palvelusivun viimeistely.

--- OhjelmistoTuotanto1/obj/Debug/net8.0-android/OhjelmistoTuotanto1.AssemblyInfo.cs
+++ OhjelmistoTuotanto1/obj/Debug/net8.0-android/OhjelmistoTuotanto1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OhjelmistoTuotanto1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9a83e4f13fb8c1ba584169066df8ca60f30e03fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4ef1f0006524f551ad4fd7d7be5c7ba249df22d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("OhjelmistoTuotanto1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OhjelmistoTuotanto1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
